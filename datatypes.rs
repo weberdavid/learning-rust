434363536d0ec8acfccd180c262f44390fd926fc
David Weber <dave.web@live.at>
2023-06-05 11:14:57 +0200

    progress

---
+++ datatypes.rs
@@ -0,0 +1,113 @@
+// interesting video: https://youtu.be/z-0-bbc80JM
+
+fn main() {
+    // INTEGERS
+    
+
+
+
+
+
+
+
+
+    // FLOATS
+
+
+
+
+
+
+
+
+
+    // BOOLEANS
+
+
+
+
+
+
+
+
+
+    // CHARS & STRINGS
+
+
+
+
+
+
+
+    // TUPLES
+
+
+
+
+
+
+
+
+
+    // ARRAYS
+
+
+
+
+
+
+
+    // STRUCTS
+
+
+
+
+
+
+
+    // ENUMS
+
+
+
+
+
+
+
+
+    //CUSTOM DATA TYPE BEHAVIOUR
+    // impl
+
+
+
+
+
+
+
+
+
+    // trait
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
